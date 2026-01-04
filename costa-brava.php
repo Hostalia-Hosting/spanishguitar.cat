4cfffdc8416d1d1116df13dc72297e2441fff841
piedracita <arnaldpedros@gmail.com>
2026-01-05 00:01:18 +0100

    update

---
+++ costa-brava.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <!-- COMMON-HEAD -->
+        <?php include "common-php/head.html"; ?>
+        <!-- /COMMON-HEAD -->
+
+        <title>Professional Spanish Guitarist for Weddings in Barcelona</title>
+
+        <meta name="description" content="PENDING DESCRIPTION">
+        <meta name="subject" content="PENDING SUBJECT">
+        <meta name="author" content="Spanish Guitar - Benja">
+        
+    </head>
+
+    <body class="home">
+
+        <!-- COMMON-HEADER/NAVIGATION -->
+        <?php include "common-php/header-navigation/en.html"; ?>
+        <!-- /COMMON-HEADER/NAVIGATION -->
+
+        <!-- COMMON-HERO -->
+        <?php include "common-php/hero/barcelona-en.html"; ?>
+        <!-- /COMMON-HERO -->
+
+        <!-- COMMON-ABOUT -->
+        <?php include "common-php/about/barcelona-en.html"; ?>
+        <!-- /COMMON-ABOUT -->
+
+        <!-- COMMON-VIDEOS -->
+        <?php include "common-php/videos/barcelona-en.html"; ?>
+        <!-- /COMMON-VIDEOS -->
+
+        <!-- COMMON-GALLERY -->
+        <?php include "common-php/gallery/en.html"; ?>
+        <!-- /COMMON-GALLERY -->
+
+        <!-- COMMON-WEDDINGS -->
+        <?php include "common-php/weddings/main-services-en.html"; ?>
+        <!-- /COMMON-WEDDINGS -->
+
+        <!-- COMMON-BOOKINGPROCESS -->
+        <?php include "common-php/booking-process/en.html"; ?>
+        <!-- /COMMON-BOOKINGPROCESS -->
+
+        <!-- COMMON-LOCATIONS -->
+        <?php include "common-php/venues/list-en.html"; ?>
+        <!-- /COMMON-LOCATIONS -->
+
+        <!-- COMMON-PRICING -->
+        <?php include "common-php/pricing/en.html"; ?>
+        <!-- /COMMON-PRICING -->
+
+        <!-- COMMON-TESTIMONIALS -->
+        <?php include "common-php/testimonials/en.html"; ?>
+        <!-- /COMMON-TESTIMONIALS -->
+         
+        <!-- COMMON-CONTACT -->
+        <?php include "common-php/contact/en.html"; ?>
+        <!-- /COMMON-CONTACT -->
+
+        <!-- COMMON-FOOTER -->
+        <?php include "common-php/footer/en.html"; ?>
+        <!-- /COMMON-FOOTER -->
+
+        <!-- COMMON-BODY -->
+        <?php include "common-php/body.html"; ?>
+        <!-- /COMMON-BODY -->
+    </body>
+</html>
